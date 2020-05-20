06b18b6385b79422ee7ebd96b23164b75ac7cf69
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2020-05-21 03:17:34 +0530

    CSV_handle commit
    To be updated

---
+++ csv_handling.py
@@ -0,0 +1,41 @@
+#Handle csv's used
+from config import *
+import pandas as pd
+import csv
+import re
+
+#function to create headers for the action set
+def add_columns(candidate_set , initial_size):
+    csvfile = 'action_document.csv'
+    with open(csvfile , 'w', newline = '') as csv_file:
+        writer = csv.DictWriter(csv_file , fieldnames = candidate_set)
+        writer.writeheader()
+
+#function to append the document list
+def append_columns(action , document, candidate_set):
+    csvfile = 'action_document.csv'
+    with open(csvfile , 'a', newline = '') as csv_file:
+        writer = csv.writer(csv_file)
+        idx = candidate_set.index(action)
+        writer.writerow([''] * (idx) + [document])
+
+append_columns('orange' , 'heyu', candidate_set)
+
+#function  to find document frequency
+def find_freq(keyword):
+    df = 0
+    csvfile = 'action_document.csv'
+    iter = 0
+    with open(csvfile) as csv_file:
+        for document in csv_file:
+            if iter == 0:
+                iter = iter + 1
+                continue
+            document = document.replace(',' , '')
+            document = document.split()
+            print(document)
+            if keyword in document:
+                df = df + 1
+
+    return df
+
